0b34d82597a44e43ad0a360d4b59e7c8093f1c5c
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-05 22:59:41 +0800

    change route / to /home, and route / is countdown page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import MainLayout from "./layout/MainLayout";
+
+import CountdownPage from "./pages/CountdownPage";
 import Home from "./pages/Home";
 import GalleryPage from "./pages/GalleryPage";
 
-export default function App() {
+export default function AppWrapper() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Pages yang memakai layout */}
+        {/* Countdown TIDAK memakai MainLayout */}
+        <Route path="/" element={<CountdownPage />} />
+
+        {/* Semua halaman utama PAKAI MAINLAYOUT */}
         <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/gallery" element={<GalleryPage />} />
         </Route>
-
-        {/* Pages lain tanpa layout bisa ditaruh di sini */}
-        {/* <Route path="/secret" element={<SecretPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
