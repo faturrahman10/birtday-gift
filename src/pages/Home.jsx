259637332e692c85a8407a9389bc8c28b69cd450
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-05 23:01:19 +0800

    remove countdown component from home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,21 @@
-// pages/Home.jsx
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Hero from "../components/Hero";
 import Timeline from "../components/Timeline";
-import Carousel from "../components/Carousel";
 import Letter from "../components/Letter";
 import Wishlist from "../components/Wishlist";
 import Quiz from "../components/Quiz";
 import TimeCapsule from "../components/TimeCapsule";
 import Footer from "../components/Footer";
 import SecretSection from "../components/SecretSection";
-import Countdown from "../components/Countdown";
 
 function Home() {
-  const [introDone, setIntroDone] = useState(false);
-
   // Init AOS animation
   useEffect(() => {
     AOS.init({ duration: 1000, once: true });
   }, []);
-
-  const startWebsite = () => {
-    // Countdown selesai ➝ langsung tampilkan konten
-    setIntroDone(true);
-  };
 
   return (
     <div
@@ -35,21 +25,14 @@ function Home() {
         dark:from-gray-900 dark:via-gray-950 dark:to-black
       "
     >
-      {!introDone ? (
-        <Countdown onFinish={startWebsite} />
-      ) : (
-        <>
-          {/* CONTENT */}
-          <SecretSection />
-          <Hero />
-          <Timeline />
-          <Letter />
-          <Wishlist />
-          <Quiz />
-          <TimeCapsule />
-          <Footer />
-        </>
-      )}
+      <SecretSection />
+      <Hero />
+      <Timeline />
+      <Letter />
+      <Wishlist />
+      <Quiz />
+      <TimeCapsule />
+      <Footer />
     </div>
   );
 }
