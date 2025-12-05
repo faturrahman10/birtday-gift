8a46702093e39750578f4115f02f0c8de63afa58
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-05 23:01:50 +0800

    add back default style page

--- src/pages/CountdownPage.jsx
+++ src/pages/CountdownPage.jsx
@@ -9,7 +9,13 @@ export default function CountdownPage() {
   };
 
   return (
-    <div>
+    <div
+      className="
+        min-h-screen transition-all duration-500
+        bg-gradient-to-br from-pink-100 via-rose-50 to-purple-100
+        dark:from-gray-900 dark:via-gray-950 dark:to-black
+      "
+    >
       <Countdown onFinish={handleFinish} />
     </div>
   );
