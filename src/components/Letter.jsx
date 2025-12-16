7b9af794cb0c5daedadf765c99842ddc2127c9a3
faturrahman10 <faturrahmanhasir@gmail.com>
2025-12-16 17:06:57 +0800

    add text info for UX feature Letter

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -23,10 +23,14 @@ const Letter = () => {
           alt="Envelope"
           className="w-40 md:w-48 mx-auto drop-shadow-lg"
         />
-        <p className="font-playfair text-2xl text-rose-300 mb-3 text-center">
-          Surat
+        <p className="font-playfair text-2xl text-rose-300 text-center">
+          Greeting Card
         </p>
       </motion.div>
+
+      <div className="mt-3 text-center text-white/80 text-xs">
+        <p>💡 Kartu ucapan dari fatur, dibaca yaah</p>
+      </div>
 
       {/* MODAL */}
       <AnimatePresence>
